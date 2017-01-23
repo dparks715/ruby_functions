9c2ac299b198b0c855cca7c92f2450e20850aa69
Daniel Parks <dparks715@yahoo.com>
2017-01-23 10:28:37 -0500

    added another test

--- subtract_test.rb
+++ subtract_test.rb
@@ -7,8 +7,13 @@ class TestMMFunctions < Minitest::Test
 		assert_equal(1,1)
 	end
 
-	def test_2_minus_one
+	def test_two_minus_one
 		assert_equal(1, subtract(2,1))
 	end
+
+	def test_one_minus_five
+		assert_equal(-4, subtract(1,5))
+	end
+
 end
 
