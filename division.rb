e90891abefa3d56e84271d81aa877f60b40f1f5f
Daniel Parks <dparks715@yahoo.com>
2017-01-23 11:19:45 -0500

    divide by zero, and divide zero

--- division.rb
+++ division.rb
@@ -1,3 +1,7 @@
 def divide(num1, num2)
-	num1 / num2
+	if num2 == 0
+		'You cannot divide by zero.'
+	else
+		num1 / num2
+	end
 end
--- division_test.rb
+++ division_test.rb
@@ -11,4 +11,12 @@ class TestDivisionFunction < Minitest::Test
 		assert_equal(2, divide(4,2))
 	end
 
+	def test_ten_div_zero
+		assert_equal('You cannot divide by zero.', divide(10,0))
+	end
+
+	def test_zero_div_five
+		assert_equal(0, divide(0,5))
+	end
+
 end
