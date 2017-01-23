697b29dcc097d00243338a448ede0098ec4aba40
Daniel Parks <dparks715@yahoo.com>
2017-01-23 14:40:36 -0500

    added test for div by 15

--- array_two.rb
+++ array_two.rb
@@ -2,10 +2,15 @@ def create_array()
 	mm_array=*(1..100)
 
 	mm_array.each_with_index do |value, index|
-		if value % 3 == 0
+		if value % 3 == 0 && value % 5 == 0
+			mm_array[index] = 'mined minds'
+
+		elsif value % 3 == 0
 			mm_array[index] = 'mined'
+
 		elsif value % 5 == 0
 			mm_array[index] = 'minds'
+
 		else value
 
 		end
--- test_array_two.rb
+++ test_array_two.rb
@@ -14,17 +14,27 @@ class TestArrayTwo < Minitest::Test
 
 	def test_one_returns_one
 		results = create_array
-		assert_equal(1, results[1-1])
+		assert_equal(1, results[0])
 	end
 
 	def test_div_by_3_is_mined
 		results = create_array
-		assert_equal('mined', results[3-1])
+		assert_equal('mined', results[2])
 	end
 
 	def test_div_by_five_is_minds
 		results = create_array
-		assert_equal('minds', results[5-1])
+		assert_equal('minds', results[4])
+	end
+
+	def test_div_by_fifteen_is_minedminds
+		results = create_array
+		assert_equal('mined minds', results[14])
+	end
+
+	def test_one_hundred_is_minds
+		results = create_array
+		assert_equal('minds', results[-1])
 	end
 
 end
